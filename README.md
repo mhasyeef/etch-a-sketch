426b380e79946513da798e90a29c5d4598038067
hasyeef <mhasyeef@gmail.com>
2022-06-08 21:52:53 +0800

    basic logic and design

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # etch-a-sketch
+
+Difficulties faced
+1) Generating the grid dynamically without manually typing the divs one by one
+2) Trying to manipulate the DOM such that the CSS properties are dynamically changed based 
+   on the desired grid size
+3) Generating random values through the Math.random()
+4) Getting the individual grids to change color 
--- script.js
+++ script.js
@@ -1,34 +1,79 @@
 const container = document.querySelector('.container')
+const slider = document.querySelector('#size')
+const btnSize = document.querySelector('#button')
+const btnRnbw = document.querySelector('#rainbow')
+const btnClear = document.querySelector('#clear')
+const colorPick = document.querySelector('#colorpicker')
 
-// function rowCol(size){
-//     for(let i = 0; i < size; i++){
-//         const div = document.createElement('div')
-//         div.classList.add('grid')
-//         container.appendChild(div)
-//     }
-// }
-
-// const grid = document.createElement('div');
-// container.appendChild(grid);                     //appends the 'grid' div under 'container'
-// grid.setAttribute('id', 'grid');                 //sets id to 'grid'
-
-// //coordinates
-// const a1 = document.createElement('div');
-// grid.appendChild(a1);
-// a1.setAttribute('id', 'a1');
-// a1.innerText = "A1";
-
-// const a2 = document.createElement('div');
-// grid.appendChild(a2);
-// a2.setAttribute('id', 'a2');
-// a2.innerText = "A2";
+let size = 32;
 
 function autoPopulate(size){
+    container.style.setProperty('--size', size)
+
     for(let i = 0; i < size*size; i++){         //size*size to get the grid total number
-        const div = document.createElement('div');
-        div.classList.add('grid');
-        container.appendChild(div);
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        container.appendChild(grid);
+       
+        grid.addEventListener('mouseover', () => {
+        grid.style.backgroundColor = "black";
+
+        });
     }
 }
 
-autoPopulate(32);
+autoPopulate(size);
+
+//to generate random color
+function generateRandomColor(){   
+    let maxVal = 0xFFFFFF;
+    let randomColor = Math.random() * maxVal;
+    randomColor = Math.floor(randomColor); //converted floating pt to integer
+    randomColor = randomColor.toString(16); //converted integer to hexadecimal
+    let randColor = randomColor.padStart(6, 0); //in the event length is < 6, adds 0 to the begining
+    return `#${randColor.toUpperCase()}`
+    
+}
+
+function reset(){
+    container.innerHTML = '';
+    
+}
+
+//to adjust size
+slider.addEventListener('input', () => {
+    let sliderVal = document.getElementById('size').value;
+    reset();
+    autoPopulate(sliderVal);
+});
+
+//color picker
+colorPick.addEventListener('input' , () => {
+    let newColor = document.getElementById('colorpicker').value;
+    let grid = container.children;
+    for(let i = 0; i  < size * size; i++){
+        grid[i].addEventListener('mouseover', (e) => {
+            console.log(grid[i]);
+            e.target.style.backgroundColor = newColor;
+        })
+    }
+})
+
+//rainbow
+btnRnbw.addEventListener('click', () => {
+    let grid = container.children;
+    for(let i = 0; i  < size * size; i++){
+        grid[i].addEventListener('mouseover', (e) => {
+            console.log(grid[i]);
+            e.target.style.backgroundColor = generateRandomColor();
+        })
+    }
+})
+
+//to clear grid
+btnClear.addEventListener('click', () => {
+    reset();
+    autoPopulate(size);
+});
+
+
