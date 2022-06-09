657c5edba2714816d1f49cea5d6b639ccaf6c678
hasyeef <mhasyeef@gmail.com>
2022-06-09 16:46:52 +0800

    fixed bugs when grid size was changed

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
-# etch-a-sketch
+Project: etch-a-sketch
 
-Difficulties faced
-1) Generating the grid dynamically without manually typing the divs one by one
-2) Trying to manipulate the DOM such that the CSS properties are dynamically changed based 
-   on the desired grid size
-3) Generating random values through Math.random()
-4) Getting the individual grids to change color 
+https://mhasyeef.github.io/etch-a-sketch/
+
+A web based etch-a-sketch game which allows you to make sketches in a pixellated style.
+
+Technologies
+
+1) HTML
+2) CSS
+3) Javascript
+4) Flexbox
+5) CSS Grid
+
+Tools
+
+1) Visual Studio Code
+2) WSL Ubuntu
+3) Git and Github
+
+What I've learnt
+
+1) Better knowledge of manipulating the DOM
+2) Generating and manipulating the different elements dynamically through the DOM
+3) Usage of Flexbox and CSS Grid to make it look clean
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const btnRnbw = document.querySelector('#rainbow')
 const btnClear = document.querySelector('#clear')
 const colorPick = document.querySelector('#colorpicker')
 
-let size = 32;
+let size = 64;
 
 function autoPopulate(size){
     container.style.setProperty('--size', size)
@@ -49,31 +49,46 @@ slider.addEventListener('input', () => {
 
 //color picker
 colorPick.addEventListener('input' , () => {
+    let sliderVal = document.getElementById('size').value;
     let newColor = document.getElementById('colorpicker').value;
-    let grid = container.children;
-    for(let i = 0; i  < size * size; i++){
-        grid[i].addEventListener('mouseover', (e) => {
-            console.log(grid[i]);
-            e.target.style.backgroundColor = newColor;
+    reset();
+    //let grid = container.children;
+    for(let i = 0; i  < sliderVal * sliderVal; i++){
+
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        container.appendChild(grid);      
+        grid.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = newColor;
+
+        // grid[i].addEventListener('mouseover', (e) => {
+        // e.target.style.backgroundColor = newColor;
         })
     }
 })
 
 //rainbow
 btnRnbw.addEventListener('click', () => {
-    let grid = container.children;
-    for(let i = 0; i  < size * size; i++){
-        grid[i].addEventListener('mouseover', (e) => {
-            console.log(grid[i]);
-            e.target.style.backgroundColor = generateRandomColor();
+    let sliderVal = document.getElementById('size').value;
+    reset();
+    for(let i = 0; i  < sliderVal * sliderVal; i++){
+        //console.log(sliderVal);
+        
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        container.appendChild(grid);
+       
+        grid.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = generateRandomColor();
         })
     }
 })
 
 //to clear grid
 btnClear.addEventListener('click', () => {
+    let sliderVal = document.getElementById('size').value;
     reset();
-    autoPopulate(size);
+    autoPopulate(sliderVal);
 });
 
 
